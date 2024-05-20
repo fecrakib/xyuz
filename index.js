1a2d67372b27aab497bc2edcdcb4edca31647e14
fecrakib <rakibislam132575@gmail.com>
2024-05-20 06:10:55 +0600

    project setup done

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose');
 
-const express=re
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+async function main() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/testProductDb');
+        console.log("data base connected")
+    } catch (error) {
+        console.log(error)
+        console.log(error.message)
+        
+    }
+
+  }
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+main();
