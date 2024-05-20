b9147030102e23987e53f6c3af8eec9d8a623fc6
fecrakib <rakibislam132575@gmail.com>
2024-05-20 10:21:29 +0600

    connected mongoes

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require('express')
 const mongoose = require('mongoose');
 
+
+
 const app = express()
-const port = 3000
+const port = 3000;
+app.use(express.json())
+app.use((express.urlencoded({extended:true})))
+// create schema
+const productSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        require:true
+    },
+    price:Number,
+    describe:String,
+    CreatedAt:{
+        type:Date,
+        default:Date.now()
+    }
+})
+// create model
+const product=mongoose.model('product',productSchema);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/products',async(req,res)=>{
+    try {
+    const {title,price,describe}=req.body;
+    // create product instance of model(row)
+    const newProduct= new product({title,price,describe});
+    // save the product ot the database
+     const ProductData=await newProduct.save()
+     res.status(2001).send(ProductData)
+    } catch (error) {
+        res.status(500).send({
+            message:error.message
+        })
+        
+    }
 })
 
 async function main() {
